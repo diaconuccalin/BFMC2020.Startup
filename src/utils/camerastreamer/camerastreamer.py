c7e8e82c0b71cdfbe1cdb8856ac0cb824657fe0e
diaconuccalin <diaconuccalin@gmail.com>
2020-02-21 21:25:55 +0200

    Test imgproc

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -142,14 +142,8 @@ class CameraStreamer(WorkerProcess):
                 width = 700
                 height = 400
 
-                print(stamps)
-                print(img.shape)
-                result, img = cv2.imencode('.jpg', img, encode_param)
-
-                print(result)
-                print(img.shape)
                 
-                #img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+                img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
                 #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 #img = img[(int(height/1.8)):(height - 30), (int(width*0.2)):(width - (int(width*0.15)))]
                 #img = cv2.GaussianBlur(img, (5,1), 0)
@@ -163,6 +157,7 @@ class CameraStreamer(WorkerProcess):
 
                 #img = cropRegion(img, np.array([roiVertices], np.int32),)
                  
+                result, img = cv2.imencode('.jpg', img, encode_param)
                 data   =  img.tobytes()
                 size   =  len(data)
 
