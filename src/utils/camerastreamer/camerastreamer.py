9543ca0348f76a010f1759f14daa9bef88115d31
diaconuccalin <diaconuccalin@gmail.com>
2020-02-21 21:26:47 +0200

    Test imgproc

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -143,10 +143,10 @@ class CameraStreamer(WorkerProcess):
                 height = 400
 
                 
-                img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+                #img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
                 #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 #img = img[(int(height/1.8)):(height - 30), (int(width*0.2)):(width - (int(width*0.15)))]
-                #img = cv2.GaussianBlur(img, (5,1), 0)
+                img = cv2.GaussianBlur(img, (5,1), 0)
                 #img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
                 #roiVertices = [
