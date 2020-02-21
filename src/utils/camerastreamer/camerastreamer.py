e0084b10e9169e92a532742584c868eaf3ec129b
diaconuccalin <diaconuccalin@gmail.com>
2020-02-21 21:19:37 +0200

    Test imgproc

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -143,9 +143,8 @@ class CameraStreamer(WorkerProcess):
                 height = 400
                 result, img = cv2.imencode('.jpg', img, encode_param)
                 
-                #img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
-                img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+                #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 #img = img[(int(height/1.8)):(height - 30), (int(width*0.2)):(width - (int(width*0.15)))]
                 #img = cv2.GaussianBlur(img, (5,1), 0)
                 #img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
