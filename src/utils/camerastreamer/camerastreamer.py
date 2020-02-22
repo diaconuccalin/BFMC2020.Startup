fac3b7de3ac80e9c88d0f0f016c1daae24a50214
diaconuccalin <diaconuccalin@gmail.com>
2020-02-22 13:20:48 +0200

    img proc testing

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -119,7 +119,7 @@ class CameraStreamer(WorkerProcess):
                 img = img[(int(height/1.8)):(height - 30), (int(width*0.2)):(width - (int(width*0.15)))]
 
                 img = cv2.GaussianBlur(img, (5,1), 0)
-                #img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
                 def cropRegion(img, vertices):
