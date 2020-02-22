a7bd658bbb4e2db355e4b15a6da477275c91742e
diaconuccalin <diaconuccalin@gmail.com>
2020-02-22 16:20:50 +0200

    img proc testing

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -119,7 +119,7 @@ class CameraStreamer(WorkerProcess):
                 img = img[(int(height/1.8)):height, 0:width]
 
                 img = cv2.GaussianBlur(img, (5,5), 0)
-                img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, -15)
+                img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 17, -5)
                 
                 #kernel = np.ones((2,2), np.uint8)
                 #img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
