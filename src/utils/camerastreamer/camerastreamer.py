460a758c49f8f3f53f116e191c0c9069a15a3c59
diaconuccalin <diaconuccalin@gmail.com>
2020-02-22 14:56:53 +0200

    img proc testing

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -120,7 +120,7 @@ class CameraStreamer(WorkerProcess):
 
                 img = cv2.GaussianBlur(img, (5,3), 0)
                 #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, -10)
-                img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, -5)
+                img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 18, -5)
                 #img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
