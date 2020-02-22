d8facadc1908cdf9aed9bb36db3c303257b9927d
diaconuccalin <diaconuccalin@gmail.com>
2020-02-22 14:37:55 +0200

    img proc testing

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -116,9 +116,9 @@ class CameraStreamer(WorkerProcess):
 
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 
-                img = img[(int(height/1.8)):(height - 30), 0:width]
+                img = img[(int(height/1.8)):height, 0:width]
 
-                img = cv2.GaussianBlur(img, (5,1), 0)
+                img = cv2.GaussianBlur(img, (5,3), 0)
                 img1, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
