c10358bcc47b734004b861e63e2299a70de40449
diaconuccalin <diaconuccalin@gmail.com>
2020-03-20 15:24:31 +0200

    ip testing

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -111,10 +111,10 @@ class CameraStreamer(WorkerProcess):
 
         def laneKeeping(img):
             height = 480
-            width = 800
+            width = 640
 
             img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            #img = img[(int(height/1.8)):height, 0:width]
+            img = img[(int(height/1.8)):height, 0:width]
             img = cv2.GaussianBlur(img, (7,7), 0)
 
             img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, -8)
