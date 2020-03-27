02aab74190e3a43eaac0c3352162de24b3fe15e7
Calin <diaconuccalin@gmail.com>
2020-03-27 23:58:01 +0200

    Custom WB - Image calibration test

--- src/hardware/camera/camerapublisher.py
+++ src/hardware/camera/camerapublisher.py
@@ -83,7 +83,7 @@ class CameraPublisher(ThreadWithStop):
         self.camera.contrast        =   0   # default
         self.camera.iso             =   0   # auto
         self.camera.awb_mode        =   'off'
-        self.camera.awb_gains       =   (1.7, 1.7)
+        self.camera.awb_gains       =   (6.7, 1.7)
         
 
         self.imgSize                =   (640, 480)    # the actual image size
