a640adee143b3ff3ddd970bf41e5f98690039c6a
Calin <diaconuccalin@gmail.com>
2020-03-28 00:23:56 +0200

    Custom WB - Image calibration test

--- src/hardware/camera/camerapublisher.py
+++ src/hardware/camera/camerapublisher.py
@@ -124,7 +124,7 @@ class CameraPublisher(ThreadWithStop):
         reds = reds / greens
         blues = blues / greens
 
-        self.camera.awb_gains = (blues, greens)
+        self.camera.awb_gains = (blues, reds)
 
     # ===================================== GET STAMP ====================================
     def _get_timestamp(self):
