f252dd8d2abb2f0b5be9cd744ca6eb73d9686505
Calin <diaconuccalin@gmail.com>
2020-03-29 15:11:03 +0300

    Lane keeping - Enable video stream

--- laneKeeping.py
+++ laneKeeping.py
@@ -85,7 +85,7 @@ class LaneKeeping(WorkerProcess):
 
                 val = laneKeeping(img)
 
-                val /= 4
+                val /= 3
 
                 val = self.pid(val)
                 
