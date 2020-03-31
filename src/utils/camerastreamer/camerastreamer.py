f59af789b881a34d90798123121bfaf8ed17ff28
diaconuccalin <diaconuccalin@gmail.com>
2020-03-31 15:06:58 +0300

    Sign Detection - Update hues

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -166,7 +166,7 @@ class CameraStreamer(WorkerProcess):
             img = cv2.erode(img, kernel, iterations=1)
             img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-            kernel = np.ones((5, 5), np.uint8)
+            #kernel = np.ones((5, 5), np.uint8)
             img = cv2.dilate(img, kernel, iterations = 1)
 
             return img
