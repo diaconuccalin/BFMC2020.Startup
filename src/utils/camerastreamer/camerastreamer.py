8b263adb801d9e4c7ad9310dd6331cca629db8f7
diaconuccalin <diaconuccalin@gmail.com>
2020-03-31 16:50:35 +0300

    Sign Detection - Save images

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -261,7 +261,7 @@ class CameraStreamer(WorkerProcess):
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 toSave = img[y:(y+h), x:(x+w)]
-                title = "/home/pi/samples/red/" + str(self.imageNumber) + "-" + current_time + ".jpg"
+                title = "/home/img/samples/red/" + str(self.imageNumber) + "-" + current_time + ".jpg"
                 print(title + str(cv2.imwrite(title, toSave)))
                 self.imageNumber += 1
 
@@ -272,7 +272,7 @@ class CameraStreamer(WorkerProcess):
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 toSave = img[y:(y+h), x:(x+w)]
-                title = "/home/pi/samples/blue/" + str(self.imageNumber) + "-" + current_time + ".jpg"
+                title = "/home/img/samples/blue/" + str(self.imageNumber) + "-" + current_time + ".jpg"
                 cv2.imwrite(title, toSave)
                 self.imageNumber += 1
             for i in range(len(yellowRectangles)):
@@ -282,7 +282,7 @@ class CameraStreamer(WorkerProcess):
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
                 toSave = img[y:(y+h), x:(x+w)]
-                title = "/home/pi/samples/yellow/" + str(self.imageNumber) + "-" + current_time + ".jpg"
+                title = "/home/img/samples/yellow/" + str(self.imageNumber) + "-" + current_time + ".jpg"
                 cv2.imwrite(title, toSave)
                 self.imageNumber += 1
 
