3516225b0c58e9b0e28a75d9abf69e0cdc827ab3
Calin <diaconuccalin@gmail.com>
2020-04-03 18:40:16 +0300

    Sign detection - tentative sign detection

--- src/utils/camerastreamer/camerastreamer.py
+++ src/utils/camerastreamer/camerastreamer.py
@@ -425,13 +425,13 @@ class CameraStreamer(WorkerProcess):
             for blueSign in blueSigns:
                 print("Try parking")
 
-                if blueSign != [] and isParking(blueSign) < 0.1:
+                if blueSign.size() > 0 and isParking(blueSign) < 0.1:
                     print("Get coords")
                     (xx, yy, ww, hh) = blueRectangles[blueSigns.index(blueSign)]
                     print("Draw rectangle")
                     original = cv2.rectangle(original, (xx, yy), (xx + ww, yy + hh), (0, 0, 255), 2)
                 print("Try crosswalk")
-                if blueSign != [] and isCrosswalk(blueSign) < 0.1:
+                if blueSign.size() > 0 and isCrosswalk(blueSign) < 0.1:
                     print("Get coords")
                     (xx, yy, ww, hh) = blueRectangles[blueSigns.index(blueSign)]
                     print("Draw rectangle")
@@ -439,7 +439,7 @@ class CameraStreamer(WorkerProcess):
 
             for redSign in redSigns:
                 print("Try stop")
-                if redSign != [] and isStop(redSign) < 0.1:
+                if redSign.size() > 0 and isStop(redSign) < 0.1:
                     print("Get coords")
                     (xx, yy, ww, hh) = redRectangles[redSigns.index(redSign)]
                     print("Draw rectangle")
@@ -447,7 +447,7 @@ class CameraStreamer(WorkerProcess):
 
             for yellowSign in yellowSigns:
                 print("Try priority")
-                if yellowSign != [] and isPriority(yellowSign) < 0.1:
+                if yellowSign.size() > 0 and isPriority(yellowSign) < 0.1:
                     print("Get coords")
                     (xx, yy, ww, hh) = yellowRectangles[yellowSigns.index(yellowSign)]
                     print("Draw rectangle")
