bb1637bca48c61a0096294ba2f21f791f4a10228
calin <diaconuccalin@gmail.com>
2020-04-06 23:29:22 +0300

    Lane keeping - Elimination round calibration

--- src/utils/remotecontrol/rcbrain.py
+++ src/utils/remotecontrol/rcbrain.py
@@ -44,7 +44,7 @@ class RcBrainConfigParams:
         speedStep : [type]
             The step value of speed
         """
-        self.maxSteerAngle = maxSteerAngle
+        self.maxSteerAngle = 30.0
         self.maxSpeed = maxSpeed
         self.steerAngleStep = steerAngleStep
         self.speedStep = speedStep
@@ -57,15 +57,15 @@ class RcBrain:
         """
 
         self.speed = 25.0
-        self.steerAngle = 0.0
+        self.steerAngle = 30.0
 
         #----------------- CONSTANT VALUES --------------------
         #this values do not change
         self.parameterIncrement =   0.1
         self.limit_configParam = RcBrainConfigParams(21.0,30.0,3.0,4.0)
 
-        self.startSpeed         =   9.0
-        self.startSteerAngle    =   1.0
+        self.startSpeed         =   0.0
+        self.startSteerAngle    =   30.0
 
         #----------------- DEFAULT VALUES ----------------------
         #when the RC is reset, this are the default values
