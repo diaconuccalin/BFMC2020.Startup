3e5149f0c9cda4ad3ac7142178bb01317c41b6af
calin <diaconuccalin@gmail.com>
2020-04-08 01:25:44 +0300

    Sign detection - Elimination round calibration

--- src/hardware/camera/camerapublisher.py
+++ src/hardware/camera/camerapublisher.py
@@ -87,7 +87,7 @@ class CameraPublisher(ThreadWithStop):
 
         time.sleep(3)
 
-        #self.camera.awb_gains = (0.95, 2.85)
+        self.camera.awb_gains = (0.95, 2.85)
 
         #vals = self.camera.awb_gains
         #print (vals)
